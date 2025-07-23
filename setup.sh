095a7413ffa809c8cc73d49843d3e39903870ba4
clnteahan <colin@teahan.us>
2025-07-23 14:25:35 -0400

    zsh and starship check

--- setup.sh
+++ setup.sh
@@ -1,3 +1,19 @@
-@echo on
+#!/bin/bash
+
+# Try to get zsh version
+if ! zsh --version > /dev/null 2>&1; then
+  echo "zsh is not installed. Exiting."
+  exit 1
+fi
+
+echo "zsh is installed."
+
+if ! starship --version > /dev/null 2>&1; then
+  echo "starship is not installed. Exiting."
+  exit 1
+fi
+
+echo "starship is installed."
+
 ln -s ~/.zsh/.zshrc ~/.zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
