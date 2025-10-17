f9249918f92ea16aed3aff2e3d663cadcdd3b046
Colin Teahan <colin@teahan.us>
2025-10-17 15:21:06 -0400

    updated script and readme

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# .zsh
+# ZSH-Config
+
+## Install:
+zsh
+starship
+
+Run:
+```
+git clone https://github.com/clnteahan/ZSH-Config.git .zsh
+cd .zsh
+./setup.sh
+```
--- setup.sh
+++ setup.sh
@@ -13,7 +13,11 @@ if ! starship --version > /dev/null 2>&1; then
   exit 1
 fi
 
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 echo "starship is installed."
 
-ln -s ~/.zsh/.zshrc ~/.zshrc
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+echo "Script dir = $SCRIPT_DIR"
+
+ln -s $SCRIPT_DIR/.zshrc ~/.zshrc
+git clone https://github.com/zsh-users/zsh-autosuggestions $SCRIPT_DIR/zsh-autosuggestions
